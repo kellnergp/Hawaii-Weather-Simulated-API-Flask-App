b4e817e670b3b140f0ea1cf55147a910f5b696d0
kellnergp <kellnergp@gmail.com>
2021-09-27 20:07:07 -0400

    app database setup

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 # import dependencies
 from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+## database setup
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.measurement
 
 # flask setup
 app = Flask(__name__)
